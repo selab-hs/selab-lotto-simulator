5a3f40c7619529f948d29a043c6a1bdf21389296
ds4pae <ds4pae@gmail.com>
2023-07-10 15:40:43 +0900

    println -> print

--- src/main/java/StartScreen.java
+++ src/main/java/StartScreen.java
@@ -1,6 +1,6 @@
 public class StartScreen {
-    public void StartScreen(){
-        System.out.println("**** Selab Lotto Simulator ****\n" +
+    public StartScreen(){
+        System.out.print("**** Selab Lotto Simulator ****\n" +
                             "1. 시작\n" +
                             "2. 종료\n" +
                             "메뉴를 입력하시오> ");

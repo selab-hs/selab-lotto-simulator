2f6c41d41374a50cd93470d6b41e3c839cc13bd3
ds4pae <ds4pae@gmail.com>
2023-07-13 20:34:35 +0900

    isNum 메소드 추가

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Input {
-    public static int getInputValue(){
+    public static int getInputValue() {
         Scanner scanner = new Scanner(System.in);
         return isValidInput(scanner.nextInt());
     }
 
-    public static int getSimulatorInput(){
+    public static int getSimulatorInput() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("시뮬레이션 할 복권 수를 입력하시오> ");
-        return isValidInput(scanner.nextInt());
+        return isNum(scanner.nextInt());
+    }
+
+    protected static int isNum(int inputNum) {
+        try {
+            String str = String.valueOf(inputNum);
+            if (!Pattern.matches("^[0-9]{1,2}$", str))
+                throw new NumberFormatException("입력된 숫자가 3자리 숫자를 넘겼어요.");
+        } catch (NumberFormatException e) {
+            System.err.println("NumberFormatException : " + e.getMessage());
+            System.exit(0);
+        }
+        return inputNum;
     }
 
     public static int isValidInput(int value) {
         try {
-            if (!Pattern.matches("^\\d$",  Integer.toString(value)))
+            if (!Pattern.matches("^\\d$", Integer.toString(value)))
                 throw new NumberFormatException("입력된 숫자는 1자리 숫자가 아닙니다.");
         } catch (NumberFormatException e) {
             System.err.println("NumberFormatException : " + e.getMessage());

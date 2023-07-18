6860b22e5d5b70a94af281a24c6195464989481d
ds4pae <ds4pae@gmail.com>
2023-07-18 22:10:52 +0900

    fix : 등수 책정 오류 해결!!

--- src/main/java/CompareNumber.java
+++ src/main/java/CompareNumber.java
@@ -2,58 +2,64 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CompareNumber {
-    private static final String[] RANKS = {"낙첨", "낙첨", "5등", "4등", "3등", "1등"};
+    private static final String[] RANKS = {"낙첨", "낙첨","낙첨", "5등", "4등", "3등"};
 
     public static String compareNumber(String randomNumber, List<String> autoNumberResult) {
-        StringBuilder sb = new StringBuilder();
-        for (String number : autoNumberResult) {
-            sb.append(number).append(" ");
-        }
-        String autoNumbersString = sb.toString().trim();
-        String[] autoNumbers = autoNumbersString.split(" ");
-        int bonusNumber = Integer.parseInt(autoNumbers[1].trim());
-        String[] numbers = randomNumber.split(" ");
         int matchCount = 0;
         boolean hasBonus = false;
+        String autoNumber = ListToString(autoNumberResult);
+        int index = autoNumber.indexOf("+");
+        int bonusNumber = Integer.parseInt(autoNumber.substring(index + 2));
+        String[] prevNumber = getPrevNumber(autoNumber);
+        String[] numbers = randomNumber.split(" ");
 
         for (String number : numbers) {
-            System.out.println("number : " + number + "numbers : " + Arrays.toString(numbers));
             int currentNumber = Integer.parseInt(number.trim());
             hasBonus = checkBonus(bonusNumber, currentNumber);
-            matchCount = checkMatch(autoNumbers[0].split(" "), currentNumber, matchCount);
+            matchCount = checkMatch(prevNumber, currentNumber, matchCount);
         }
 
+        String rank = checkRank(matchCount,hasBonus);
+        System.out.println("맞은 갯수: " + matchCount);
+        return rank;
+    }
+
+    private static String checkRank(int matchCount, boolean hasBonus) {
         String rank = RANKS[matchCount];
         if (matchCount == 4 && hasBonus) {
             rank = "2등";
         }
-
-        System.out.println("맞은 갯수: " + matchCount + ", 등수: " + rank);
+        if(matchCount == 5){
+            rank = "1등";
+        }
         return rank;
     }
 
-   /* private static int checkMatch(String[] autoNumbers, int currentNumber, int matchCount) {
-        for (String autoNumber : autoNumbers) {
-            int winningNumber = Integer.parseInt(autoNumber.trim());
-            System.out.println("당첨 : " + winningNumber + "내 숫자 : " + currentNumber);
-            if (currentNumber == winningNumber) {
-                return matchCount + 1;
-            }
-        }
-        return matchCount;
+    private static String[] getPrevNumber(String autoNumber) {
+        String[] index1 = autoNumber.split("\\+");
+        String[] index2 = index1[0].split(" ");
+
+        return index2;
     }
-*/
+
+    private static String ListToString(List<String> autoNumberResult) {
+
+        StringBuilder sb = new StringBuilder();
+        for (String number : autoNumberResult) {
+            sb.append(number).append(" ");
+        }
+        return sb.toString().trim();
+    }
+
     private static int checkMatch(String[] autoNumbers, int currentNumber, int matchCount) {
-        for (String autoNumber : autoNumbers) {
-            String[] numberArray = autoNumber.split(" ");
-            for (String number : numberArray) {
+            for (String number : autoNumbers) {
                 int winningNumber = Integer.parseInt(number.trim());
+                //System.out.println("당첨 번호 : " + winningNumber + ", 내 번호 : " + currentNumber);
                 if (currentNumber == winningNumber) {
                     matchCount++;
                     break;
                 }
             }
-        }
         return matchCount;
     }
 

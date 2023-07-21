883f51d9994005fa0214494b11bacfad8b9ebee5
ds4pae <ds4pae@gmail.com>
2023-07-21 20:49:40 +0900

    feat : 최종 결과 출력문 추가

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,11 +1,22 @@
 import java.util.List;
 
 public class Output {
+    private static int[] ranks = new int[6];
+
     public static void startScreen(){
         System.out.print("**** Selab Lotto Simulator ****\n" +
                             "1. 시작\n" +
                             "2. 종료\n" +
                             "메뉴를 입력하시오> ");
+    }
+
+    public static void ExitScreen(){
+        for(int i = 0 ; i < ranks.length - 1; i++){
+            System.out.print((i + 1) + "등 : " + ranks[i] +", ");
+        }
+        System.out.println("낙첨 : " + ranks[5]);
+        System.out.println("************* END *************");
+        System.exit(0);
     }
 
     public static void autoNumberScreen(List<String> autoNumberResult) {
@@ -27,5 +38,29 @@ public class Output {
         //String.join(",",autoNumberResult)
         String randomNumberResult = number + "  -> " + rank;
         System.out.println(randomNumberResult);
+    }
+
+    public static void setRankResult(int matchCount, boolean hasBonus) {
+        switch (matchCount) {
+            case 6:
+                ranks[0]++; // 1등
+                break;
+            case 5:
+                if (hasBonus) {
+                    ranks[1]++; // 2등
+                } else {
+                    ranks[2]++; // 3등
+                }
+                break;
+            case 4:
+                ranks[3]++; // 4등
+                break;
+            case 3:
+                ranks[4]++; // 5등
+                break;
+            default:
+                ranks[5]++; // 낙첨
+                break;
+        }
     }
 }

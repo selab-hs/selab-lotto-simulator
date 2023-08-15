907653422068ced4ed0c2eb7ce5fe12f79c7657b
ds4pae <ds4pae@gmail.com>
2023-08-15 16:06:15 +0900

    refactor : ranks 배열 삭제에 의한 exitScreen 로직 변경

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -1,8 +1,6 @@
 import java.util.List;
 
 public class Output {
-    private static int[] ranks = new int[6];
-
     public static void startScreen(){
         System.out.print("**** Selab Lotto Simulator ****\n" +
                             "1. 시작\n" +
@@ -10,11 +8,11 @@ public class Output {
                             "메뉴를 입력하시오> ");
     }
 
-    public static void ExitScreen(){
-        for(int i = 0 ; i < ranks.length - 1; i++){
-            System.out.print((i + 1) + "등 : " + ranks[i] +", ");
+    public static void exitScreen(){
+        for(int i = 0 ; i < Rank.ranks.length - 1; i++){
+            System.out.print((i + 1) + "등 : " + Rank.ranks[i] +", ");
         }
-        System.out.println("낙첨 : " + ranks[5]);
+        System.out.println("낙첨 : " + Rank.ranks[5]);
         System.out.println("************* END *************");
         System.exit(0);
     }
@@ -40,27 +38,5 @@ public class Output {
         System.out.println(randomNumberResult);
     }
 
-    public static void setRankResult(int matchCount, boolean hasBonus) {
-        switch (matchCount) {
-            case 6:
-                ranks[0]++; // 1등
-                break;
-            case 5:
-                if (hasBonus) {
-                    ranks[1]++; // 2등
-                } else {
-                    ranks[2]++; // 3등
-                }
-                break;
-            case 4:
-                ranks[3]++; // 4등
-                break;
-            case 3:
-                ranks[4]++; // 5등
-                break;
-            default:
-                ranks[5]++; // 낙첨
-                break;
-        }
-    }
+
 }

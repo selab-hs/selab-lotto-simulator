a656f51aa17830df214e3c960d0493c1e040668e
ds4pae <ds4pae@gmail.com>
2023-08-24 15:52:50 +0900

    test : feat initial tests

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,34 +1,49 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * 확인 후 해당 파일을 삭제하시기 바랍니다.
  */
 public class ExampleTest {
-    private Example example;
-
+    @DisplayName("입력 값에 따른 로또 개수 확인")
     @Test
-    public void 성공_테스트() {
+    public void testLottoNumberGenerationCount() {
         // given
-        String n = "10";
-        example = new Example(n);
+        int given = 10;
 
         // when
-        int actual = example.getN();
-        int expected = 10;
+        RandomNumber randomNumber = new RandomNumber(given);
+        List<String> lottoNumber = RandomNumber.createRandomNumbers(given);
+        int actual = lottoNumber.size();
+        int expected = 20;
 
         // then
         assertEquals(expected, actual);
     }
 
+    @DisplayName("랜덤 번호 개수 확인")
     @Test
-    public void 실패_테스트() {
+    public void testRandomNumberCount() {
         // given
-        String n = "나숫자 아닌디ㅋ";
+        int given = 1;
 
-        // when, then
-        Assertions.assertThrows(NumberFormatException.class, () -> example = new Example(n));
+        // when
+        RandomNumber randomNumber = new RandomNumber(given);
+        List<String> lottoNumber = RandomNumber.createRandomNumbers(given);
+        String number = lottoNumber.get(0);
+        int actual = number.length();
+        int expected = 7;
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @DisplayName("번호 비교 로직 확인")
+    @Test
+    public void testCompareNumber() {
+
     }
 }

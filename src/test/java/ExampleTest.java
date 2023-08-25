49c7b492b85d788932835152bfeb1903b3c82bd3
ds4pae <ds4pae@gmail.com>
2023-08-25 15:35:45 +0900

    test : feat initial tests

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -44,6 +46,33 @@ public class ExampleTest {
     @DisplayName("번호 비교 로직 확인")
     @Test
     public void testCompareNumber() {
+        // given
+        List<String> randomNumber = new ArrayList<String>();
+        randomNumber.add("1 2 3 4 5 6 7");
+        String autoNumber = "1 2 3 4 5 6 7";
 
+        // when
+        CompareNumber compareNumber = new CompareNumber();
+        CompareNumber.checkRank(randomNumber, autoNumber);
+
+        // then
+        //checkRank 리턴 값이 void 이기 때문에, 다른 방법을 고안해보아야 할 것 같다.
     }
+
+    @DisplayName("로또 번호 값이 1~45의 중복되지 않는 값을 지키는 지 확인")
+    @Test
+    public void testValid() {
+        // given
+
+        List<String> given = new ArrayList<String>();
+        given.add("1 2 3 4 5 6 + 7");
+
+        // when
+        AutoNumber autoNumber = new AutoNumber();
+        List<String> autoNumberResult = AutoNumber.createAutoNumberResult(AutoNumber.createRandomNumbers());
+        // then
+        //비교 로직 생각
+    }
+
+
 }
